3c39d1b9563c2e04f0adc544c53bbeff152b2467
Ahmed Abdelaal <a.abdelaal@protonmail.com>
2022-03-11 11:20:15 +0300

    Added CORS support

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,21 @@
-import express, { Request, Response } from 'express'
-import bodyParser from 'body-parser'
+import express, { Request, Response } from 'express';
+import cors from 'cors';
+import bodyParser from 'body-parser';
 
 const app: express.Application = express()
-const address: string = "0.0.0.0:3000"
+const port: number = 3000;
 
-app.use(bodyParser.json())
+const corsOptions = {
+    origin: 'http://google.com' // Assuming google will consume our Api
+}
+
+app.use(cors(corsOptions));
+app.use(bodyParser.json());
 
 app.get('/', function (req: Request, res: Response) {
     res.send('Hello World!')
 })
 
 app.listen(3000, function () {
-    console.log(`starting app on: ${address}`)
+    console.log(`server started at http://localhost:${port}`);
 })
