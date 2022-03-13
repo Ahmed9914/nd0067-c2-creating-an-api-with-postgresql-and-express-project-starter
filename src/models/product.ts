228494e31ea4ea8223757b292c7e19df557590ef
Ahmed Abdelaal <a.abdelaal@protonmail.com>
2022-03-13 20:39:10 +0300

    Added create product functionality and required tests

--- src/models/product.ts
+++ src/models/product.ts
@@ -1,6 +1,7 @@
 import { client } from '../database';
 
 export type Product = {
+    id?: number;
     name: string;
     price: number;
 };
@@ -17,4 +18,20 @@ export class ProductStore {
             throw new Error(`Can't get products ${error}`);
         }  
     }
+
+    async create(product: Product): Promise<Product> {
+        try {
+            const sql = 'INSERT INTO products (name, price) VALUES($1, $2) RETURNING *';
+            const connection = await client.connect();
+            const result = await connection.query(sql, [product.name, product.price]);
+            const createdProduct = result.rows[0];
+            connection.release();
+            return createdProduct;
+        } catch (error) {
+            throw new Error(`Could not add new product ${product.name}: ${error}`);
+        }
+    }
+
+     
+    
 }
--- src/tests/productSpec.ts
+++ src/tests/productSpec.ts
@@ -1,8 +1,11 @@
 import { Product, ProductStore } from "../models/product";
 import supertest from 'supertest';
 import app from '../server';
+import { User, UserStore } from "../models/user";
+import jwt from 'jsonwebtoken';
 
 const productStore = new ProductStore();
+const userStore = new UserStore();
 
 const request = supertest(app);
 
@@ -13,13 +16,46 @@ describe("Products tests", () => {
             const result = await productStore.index();
             expect(result).toEqual([]);
             });
+        
+        it('should add a new product to products table', async () => {
+            const product: Product = {
+                name: "shoes",
+                price: 20
+            };
+
+            const newProduct = await productStore.create(product);
+            expect(newProduct.id).toBe(1);
+        })
     });
 
     describe('products endpoint requests tests', () => {
-        it('request to index returns empty list before creating any products', async () => {
+        it('request to index returns created products', async () => {
             const response = await request.get('/products');
-            expect(response.text).toBe('[]');
+            expect(response.text).toBe('[{"id":1,"name":"shoes","price":"20"}]');
         });
+
+        it('should create a new product if a jwt token submitted', async () => {
+                const productCreator: User = {
+                    firstname: 'create',
+                    lastname: 'products',
+                    password: '123456'
+                };
+                const productCreatorResponse = await userStore.create(productCreator);
+                
+                jwt.sign({ user: productCreatorResponse}, process.env.TOKEN_SECRET as string, async (err: unknown, genToken: unknown) => {
+                    const authToken = await genToken;
+                    const response = await request
+                    .post('/products')
+                    .send({
+                        name: 'food',
+                        price: 10,
+                        token: authToken
+                    });
+                    expect(response.statusCode).toBe(201);
+            });
+        });
+
+
     });
 
 });
